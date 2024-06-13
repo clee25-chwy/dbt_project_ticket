88939487cb03d5579de9227f703301eed0dd8abe
clee25 <fermat.leee@gmail.com>
2024-06-13 11:54:45 -0400

    Create production db -- prod_nyc...db. You can use dbt compile --target prod to run only prod, otherwise it will run test. + prod info into profiles.yml.

--- get_the_data.py
+++ get_the_data.py
@@ -9,27 +9,27 @@ SHOW TABLES
 
 # SELECT * FROM parking_violation_codes limit 5
 
-with duckdb.connect('data/nyc_parking_violations.db') as con:
+with duckdb.connect('data/prod_nyc_parking_violations.db') as con:
     print(con.sql(sql_query).df())
 
-sql_query_import_1 = '''
-CREATE OR REPLACE TABLE parking_violation_codes AS
-SELECT *
-FROM read_csv_auto(
-    'data/dof_parking_violation_codes.csv',
-    normalize_names=True
-)
-'''
-# normalize_names -> add _ in all columns name if there is space
-sql_query_import_2 = '''
-CREATE OR REPLACE TABLE parking_violations_2023 AS
-SELECT *
-FROM read_csv_auto(
-    'data/parking_violations_issued_fiscal_year_2023_sample.csv',
-    normalize_names=True
-)
-'''
-
-with duckdb.connect('data/nyc_parking_violations.db') as con:
-    con.sql(sql_query_import_1)
-    con.sql(sql_query_import_2)
+# sql_query_import_1 = '''
+# CREATE OR REPLACE TABLE parking_violation_codes AS
+# SELECT *
+# FROM read_csv_auto(
+#     'data/dof_parking_violation_codes.csv',
+#     normalize_names=True
+# )
+# '''
+# # normalize_names -> add _ in all columns name if there is space
+# sql_query_import_2 = '''
+# CREATE OR REPLACE TABLE parking_violations_2023 AS
+# SELECT *
+# FROM read_csv_auto(
+#     'data/parking_violations_issued_fiscal_year_2023_sample.csv',
+#     normalize_names=True
+# )
+# '''
+#
+# with duckdb.connect('data/prod_nyc_parking_violations.db') as con:
+#     con.sql(sql_query_import_1)
+#     con.sql(sql_query_import_2)
